4e52ef0e4ddce7f4ecc3548db66f09eaa984ef91
jeseekm <72486997+jeseekm@users.noreply.github.com>
2021-09-03 10:04:31 +0300

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -152,5 +152,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 # EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'joywholesale.kasarani@gmail.com'
-EMAIL_HOST_PASSWORD  = 'Joytotheworld987'
+EMAIL_HOST_PASSWORD  = ''
 EMAIL_USE_TLS = True
